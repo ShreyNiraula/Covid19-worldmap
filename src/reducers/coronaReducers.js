c38c23a7e596ea8dc82c02d043ac4a5474b3a9ca
Shrey Niraula <nshrey53@gmail.com>
2020-07-31 21:04:11 +0545

    switch case with actiontypes in coronareducer.js

--- src/reducers/coronaReducers.js
+++ src/reducers/coronaReducers.js
@@ -3,9 +3,19 @@ import mapOptions from "../mapOptions/mapOptions";
 const initialState = {
   isLoading: false,
   mapOptions: mapOptions,
-
-  
 };
 export const coronaReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case "CORONA_WAITING":
+    //
+
+    case "CORONA_SUCCESSFUL":
+    //
+
+    case "CORONA_REJECTED":
+    //
+
+    default:
       return state;
+  }
 };
