0b8ac21a36a025924925da6129ada50078a2a1c3
Shrey Niraula <nshrey53@gmail.com>
2020-07-31 21:19:12 +0545

    Totalcases, total death counts and totalrecovered all finished

--- src/actions/coronaActions.js
+++ src/actions/coronaActions.js
@@ -1,4 +1,3 @@
-import { Axis } from "highcharts";
 import Axios from "axios";
 
 export const getData = ({ buttonType }) => async (dispatch) => {
@@ -8,20 +7,54 @@ export const getData = ({ buttonType }) => async (dispatch) => {
     });
 
     const countryWise = [];
+    var color='';
+    var subtitle='';
+    var name='';
     const resp = await Axios.get(`https://api.covid19api.com/summary`);
     resp.data.Countries.forEach((country) => {
-        var subarr = [];
+
+      var subarr = [];
+
+      if (buttonType === "cases") {
         subarr.push(country.CountryCode.toLowerCase());
         subarr.push(country.TotalConfirmed);
         countryWise.push(subarr);
+        color='blue';
+        subtitle='Total Number of Cases';
+        name='Total cases';
+
+      } else if (buttonType === "deaths") {
+        subarr.push(country.CountryCode.toLowerCase());
+        subarr.push(country.TotalDeaths);
+        countryWise.push(subarr);
+        color='red';
+        subtitle='Total Death Counts';
+        name ='Total Deaths'
+
+      } else {
+        subarr.push(country.CountryCode.toLowerCase());
+        subarr.push(country.TotalRecovered);
+        countryWise.push(subarr);
+        color='green';
+        subtitle='Total Recovered Counts';
+        name='Total Recovered';
+      }
     });
+
+    console.log('coun', countryWise);
+    console.log('col', color);
+    console.log('sub', subtitle);
+    
+
+
+
     dispatch({
       type: "CORONA_SUCCESSFUL",
       payload: {
         countryWise,
-        color:'blue',
-        subtitle:'Total Number of Cases',
-        name:'Total cases'
+        color,
+        subtitle,
+        name,
         // maxCases
       },
     });
