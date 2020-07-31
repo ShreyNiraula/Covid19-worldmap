fb22a620795626ab6df66e40bfacbac0036c85a1
Shrey Niraula <nshrey53@gmail.com>
2020-07-31 21:06:11 +0545

    IMMUTABLE logic added in coronaReducers.js

--- src/reducers/coronaReducers.js
+++ src/reducers/coronaReducers.js
@@ -1,4 +1,5 @@
 import mapOptions from "../mapOptions/mapOptions";
+import mapData from '../mapData/mapData'
 
 const initialState = {
   isLoading: false,
@@ -7,13 +8,53 @@ const initialState = {
 export const coronaReducers = (state = initialState, action) => {
   switch (action.type) {
     case "CORONA_WAITING":
-    //
+        return { ...state, isLoading: true };
+
 
     case "CORONA_SUCCESSFUL":
-    //
+        return {
+            ...state,
+            isLoading: false,
+            mapOptions: {
+              ...state,
+    
+              series: [
+                {
+                  mapData: mapData, //static data
+                  states: {
+                    hover: {
+                      color: action.payload.color,
+                    },
+                  },
+                  name: action.payload.name,
+                  dataLabels: {
+                    enabled: true,
+                    format: "{point.name}",
+                  },
+                  data: action.payload.countryWise,
+                },
+              ],
+              title: {
+                text: "Worldwise Corona Cases",
+              },
+              subtitle: {
+                text: action.payload.subtitle,
+              },
+              mapNavigation: {
+                enabled: true,
+                buttonOptions: {
+                  verticalAlign: "bottom",
+                },
+              },
+              colorAxis: {
+                min: 0
+              },
+            },
+          };
 
     case "CORONA_REJECTED":
-    //
+        return { ...state, isLoading: false };
+        
 
     default:
       return state;
